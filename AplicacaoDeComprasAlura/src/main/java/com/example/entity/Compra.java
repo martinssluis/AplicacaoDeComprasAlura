e495c668d8a552e4a3347cf5ecf03ae7bb5c96db
Luis Martins <luishenrique.martinslima@gmail.com>
2026-08-06 15:02:51 -0300

    feat(Compra): cria classe Compra com atributos nome e valor, e cria metodo realizarCompra com verificação de limite de valor

--- AplicacaoDeComprasAlura/src/main/java/com/example/entity/Compra.java
+++ AplicacaoDeComprasAlura/src/main/java/com/example/entity/Compra.java
@@ -1,4 +1,43 @@
 package com.example.entity;
 
 public class Compra {
+    private String nome;
+    private Double valor;
+
+    public Compra() {}
+
+    public Compra(String nome, Double valor) {
+        this.nome = nome;
+        this.valor = valor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+   public void realizarCompra(Cartao cartao){
+        if (this.valor > cartao.getLimite()){
+            System.out.println("Saldo insuficiente!");
+        }else {
+            System.out.println("Compra realizada com sucesso! " + this.getNome() + " no valor de R$" + this.valor);
+            cartao.setLimite(cartao.getLimite() - this.valor);
+        }
+   }
+
+    @Override
+    public String toString() {
+        return "Nome: "+this.nome + " no valor de R$"+this.valor;
+    }
 }
