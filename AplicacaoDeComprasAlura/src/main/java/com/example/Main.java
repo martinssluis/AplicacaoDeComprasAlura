b1ed328c0d4fcb3979d60e284d407d16025a4fc3
Luis Martins <luishenrique.martinslima@gmail.com>
2026-08-06 15:04:38 -0300

    feat(Main): cria aruivo principal com instancias de Limite, Compra e Cartao

--- AplicacaoDeComprasAlura/src/main/java/com/example/Main.java
+++ AplicacaoDeComprasAlura/src/main/java/com/example/Main.java
@@ -1,7 +1,39 @@
 package com.example;
 
+import com.example.entity.Cartao;
+import com.example.entity.Compra;
+import com.example.entity.Menu;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        Scanner sc = new Scanner(System.in);
+
+        Cartao cartaoDeCretido = new Cartao();
+        System.out.println("Digite o limite do cartão: ");
+        cartaoDeCretido.setLimite(sc.nextDouble());
+        sc.nextLine();
+
+        Compra compra = new Compra();
+        System.out.println("Nome da compra: ");
+        compra.setNome(sc.nextLine());
+        sc.nextLine();
+        System.out.println("Valor da compra: ");
+        compra.setValor(sc.nextDouble());
+        compra.realizarCompra(cartaoDeCretido);
+        System.out.println("Novo Limite: " + cartaoDeCretido.getLimite());
+
+        Menu menuInicial = new Menu();
+        menuInicial.exibirMenu(1);
+
+        List<Compra> extratoCompras = new LinkedList<>();
+        extratoCompras.add(compra);
+        System.out.println(extratoCompras);
+
+
     }
 }
