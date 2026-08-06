a3075be89c2aabed4cfe0917d6220b397b717bb1
Luis Martins <luishenrique.martinslima@gmail.com>
2026-08-06 15:01:39 -0300

    feat(Cartao): cria classe com atributo limite

---
+++ AplicacaoDeComprasAlura/src/main/java/com/example/entity/Cartao.java
@@ -0,0 +1,13 @@
+package com.example.entity;
+
+public class Cartao {
+    private Double limite;
+
+    public Double getLimite() {
+        return limite;
+    }
+
+    public void setLimite(Double limite) {
+        this.limite = limite;
+    }
+}
