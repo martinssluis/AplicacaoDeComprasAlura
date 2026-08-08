d2fce5d140a2e0f69a5e932efd09310141d98f4d
Luis Martins <luishenrique.martinslima@gmail.com>
2026-08-08 13:29:38 -0300

    fix(Compra): muda atributo nome para descricao e apaga os setters

--- AplicacaoDeComprasAlura/src/main/java/com/example/entity/Compra.java
+++ AplicacaoDeComprasAlura/src/main/java/com/example/entity/Compra.java
@@ -1,43 +1,27 @@
 package com.example.entity;
 
 public class Compra {
-    private String nome;
+    private String descricao;
     private Double valor;
 
-    public Compra() {}
-
-    public Compra(String nome, Double valor) {
-        this.nome = nome;
+    // sempre que informarmos descricao e valor, criaremos uma nova compra
+    public Compra(String descricao, Double valor) {
+        this.descricao = descricao;
         this.valor = valor;
     }
 
-    public String getNome() {
-        return nome;
+    public String getDescricao() {
+        return descricao;
     }
 
     public Double getValor() {
         return valor;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public void setValor(Double valor) {
-        this.valor = valor;
-    }
-
-   public void realizarCompra(Cartao cartao){
-        if (this.valor > cartao.getLimite()){
-            System.out.println("Saldo insuficiente!");
-        }else {
-            System.out.println("Compra realizada com sucesso! " + this.getNome() + " no valor de R$" + this.valor);
-            cartao.setLimite(cartao.getLimite() - this.valor);
-        }
-   }
 
     @Override
     public String toString() {
-        return "Nome: "+this.nome + " no valor de R$"+this.valor;
+        return "Compra : \n" +
+        "Descricao: "+this.descricao + " no valor de R$"+this.valor;
     }
 }
