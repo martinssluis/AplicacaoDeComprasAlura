2ec9cf14864af949bebde3ce4c765872da8a58c7
Luis Martins <luishenrique.martinslima@gmail.com>
2026-08-08 13:32:02 -0300

    fix(Main): limpa arquivo principal e coloca logica me menu

--- AplicacaoDeComprasAlura/src/main/java/com/example/Main.java
+++ AplicacaoDeComprasAlura/src/main/java/com/example/Main.java
@@ -2,10 +2,7 @@ package com.example;
 
 import com.example.entity.Cartao;
 import com.example.entity.Compra;
-import com.example.entity.Menu;
 
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -13,27 +10,21 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
 
-        Cartao cartaoDeCretido = new Cartao();
-        System.out.println("Digite o limite do cartão: ");
-        cartaoDeCretido.setLimite(sc.nextDouble());
-        sc.nextLine();
+        System.out.print("Digite o limite do cartão: ");
+        double limite = sc.nextDouble();
+        Cartao cartaoDeCretido = new Cartao(limite);
 
-        Compra compra = new Compra();
-        System.out.println("Nome da compra: ");
-        compra.setNome(sc.nextLine());
-        sc.nextLine();
-        System.out.println("Valor da compra: ");
-        compra.setValor(sc.nextDouble());
-        compra.realizarCompra(cartaoDeCretido);
-        System.out.println("Novo Limite: " + cartaoDeCretido.getLimite());
-
-        Menu menuInicial = new Menu();
-        menuInicial.exibirMenu(1);
-
-        List<Compra> extratoCompras = new LinkedList<>();
-        extratoCompras.add(compra);
-        System.out.println(extratoCompras);
-
-
+        int opcao = 1;
+        do{
+            System.out.print("Digite a descrição da compra: ");
+            String descricaoCompra = sc.nextLine();
+            sc.nextLine();
+            System.out.print("Digite o valor da compra: ");
+            double valorCompra = sc.nextDouble();
+            Compra compra = new Compra(descricaoCompra, valorCompra);
+            cartaoDeCretido.realizarCompra(compra);
+            System.out.print("Digite 0 para sair ou 1 para continuar: ");
+            opcao = sc.nextInt();
+        }while(opcao!=0);
     }
 }
