0abce13f3126920ff14c6f13d95612039897883a
Luis Martins <luishenrique.martinslima@gmail.com>
2026-08-08 14:14:27 -0300

    feat(Compra): implementa Comparable<Compra> para comprar outra compra com a propria intancia de compra atraves do valor, dessa forma temos uma ordenação do maior valor para o menor

--- AplicacaoDeComprasAlura/src/main/java/com/example/entity/Compra.java
+++ AplicacaoDeComprasAlura/src/main/java/com/example/entity/Compra.java
@@ -1,6 +1,6 @@
 package com.example.entity;
 
-public class Compra {
+public class Compra implements Comparable<Compra>{
     private String descricao;
     private Double valor;
 
@@ -22,5 +22,10 @@ public class Compra {
     @Override
     public String toString() {
         return "Compra - Descricao: "+ this.getDescricao() + " no valor de R$"+this.valor;
+    }
+
+    @Override
+    public int compareTo(Compra outraCompra) {
+        return outraCompra.getValor().compareTo(this.getValor());
     }
 }
